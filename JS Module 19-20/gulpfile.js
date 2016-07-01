5bd341649880c3ec8010f09334b1640d79b834ef
Pasha G <paul.gladoon@gmail.com>
2016-07-02 00:07:50 +0300

    change homework 19-20

--- JS Module 19-20/gulpfile.js
+++ JS Module 19-20/gulpfile.js
@@ -22,7 +22,7 @@ gulp.task('sass', function () {
 gulp.task('scripts', function() {
   return gulp.src([
     'app/libs/jquery/dist/jquery.min.js',
-    'app/libs/magnific-popup/dist/jquery.magnific-popup.min.js',
+    // 'app/libs/magnific-popup/dist/jquery.magnific-popup.min.js',
   ])
   .pipe(concat('libs.min.js'))
   .pipe(uglify())
